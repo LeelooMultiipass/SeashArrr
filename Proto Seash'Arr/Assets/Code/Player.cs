87d085656f379ae8a63243ab5818c85bf6b4d7de
Jadesardier68 <jade.sardier@e-artsup.net>
2024-12-16 15:29:56 +0100

    Multiplayer fonctionnel

--- Proto Seash'Arr/Assets/Code/Player.cs
+++ Proto Seash'Arr/Assets/Code/Player.cs
@@ -9,13 +9,16 @@ public class Player : MonoBehaviour
     public enum Role
     {
         Captain,
-        Cook,
-        Carpenter,
-        Engineer,
+        //Cook,
+       // Carpenter,
+       // Engineer,
         Doctor,
         Fighter,
-        Cannonier
+        //Cannonier
     }
+
+    public List<Material> CharactersRoles;
+    private static int roleIndex = 0;
 
     [SerializeField] private int HPMax;
     public Role role;
@@ -30,22 +33,58 @@ public class Player : MonoBehaviour
         {
             case Role.Captain:
                 break;
-            case Role.Cook:
-                break;
-            case Role.Carpenter:
-                break;
-            case Role.Engineer:
-                break;
+            //case Role.Cook:
+              //  break;
+            //case Role.Carpenter:
+              //  break;
+            //case Role.Engineer:
+              //  break;
             case Role.Doctor:
                 break;
             case Role.Fighter:
                 break;
-            case Role.Cannonier:
-                break;
+            //case Role.Cannonier:
+              //  break;
             default:
                 throw new ArgumentOutOfRangeException();
         }
-        
+
+        HP = HPMax;
+        AssignRole();
+        AssignMaterial();
+
+    }
+
+    private void AssignRole()
+    {
+        // Définir le rôle basé sur l'index global
+        role = (Role)roleIndex;
+
+        // Incrémenter l'index global et revenir au début si nécessaire
+        roleIndex = (roleIndex + 1) % Enum.GetValues(typeof(Role)).Length;
+    }
+
+    private void AssignMaterial()
+    {
+        // Vérifier que la liste des matériaux contient suffisamment d'éléments
+        if (CharactersRoles.Count > 0 && TryGetComponent<Renderer>(out Renderer renderer))
+        {
+            int roleMaterialIndex = (int)role;
+
+            if (roleMaterialIndex >= 0 && roleMaterialIndex < CharactersRoles.Count)
+            {
+                // Appliquer le matériau correspondant au rôle
+                renderer.material = CharactersRoles[roleMaterialIndex];
+            }
+            else
+            {
+                Debug.LogWarning("Pas de matériau disponible pour ce rôle.");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Pas de matériaux ou Renderer non trouvé.");
+        }
     }
 
     public int GetHP()
