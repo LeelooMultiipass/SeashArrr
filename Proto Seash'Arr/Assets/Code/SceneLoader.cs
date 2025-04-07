b70de4e93c690b9dda78111ff4aa00708434ee9a
Jadesardier68 <jade.sardier@e-artsup.net>
2025-04-07 08:47:47 +0200

    Push avant début PFA

--- Proto Seash'Arr/Assets/Code/SceneLoader.cs
+++ Proto Seash'Arr/Assets/Code/SceneLoader.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public InputActionAsset inputActionAsset;
+
+    private InputActionMap NavigationMap;
+    private void Start()
+    {
+      
+    }
+    
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+        if (sceneName == "Test Lylou")
+        {
+            NavigationMap = inputActionAsset.FindActionMap("GameplayNavigation");  
+        }
+        
     }
 }
