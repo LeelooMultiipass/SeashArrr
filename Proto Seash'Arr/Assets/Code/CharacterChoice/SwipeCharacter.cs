36f429cba243eab7c866f626a5f8fb108ff18f1c
Jadesardier68 <jade.sardier@e-artsup.net>
2025-04-07 10:55:54 +0200

    Dev Choix Character

--- Proto Seash'Arr/Assets/Code/CharacterChoice/SwipeCharacter.cs
+++ Proto Seash'Arr/Assets/Code/CharacterChoice/SwipeCharacter.cs
@@ -5,58 +5,131 @@ using UnityEngine.InputSystem;
 
 public class SwipeCharacter : MonoBehaviour
 {
-    public List<GameObject> characters;
-    private int currentIndex = 0;
-    public InputAction Left;
-    public InputAction Right;
+    public List<GameObject> charactersPlayer2;
+    public List<GameObject> charactersPlayer3;
+    private int currentIndexPlayer2 = 0;
+    private int currentIndexPlayer3 = 0;
+    private PlayerInput playerInput;
 
-    
+    void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+    }
+
     void Start()
     {
-        // Assure que seul le premier personnage est actif au début
-        UpdateCharacterVisibility();
+        if (playerInput.playerIndex == 0)
+        {
+            // Désactive les personnages du joueur 2 (Player 3)
+            foreach (var character in charactersPlayer3)
+            {
+                character.SetActive(false);
+            }
+
+            // Affiche les personnages du joueur 1 (Player 2)
+            UpdateCharacterVisibility2();
+        }
+        // Si c'est le joueur 2 (playerIndex == 1)
+        else if (playerInput.playerIndex == 1)
+        {
+            // Désactive les personnages du joueur 1 (Player 2)
+            foreach (var character in charactersPlayer2)
+            {
+                character.SetActive(false);
+            }
+
+            // Affiche les personnages du joueur 2 (Player 3)
+            UpdateCharacterVisibility3();
+        }
+
     }
 
-    private void OnEnable()
+    public void OnRight(InputAction.CallbackContext context)
     {
-        Left.Enable();
-        Right.Enable();
-        Left.started += ChangeCharacterDown;
-        Right.started += ChangeCharacterUp;
+        if (playerInput == null)
+        {
+            Debug.LogWarning("PlayerInput is null on OnRight.");
+            return;
+        }
+
+        if (context.started)
+        {
+            Debug.Log($"Player {playerInput.playerIndex} pressed RIGHT");
+            ChangeCharacterUp(context);
+        }
     }
 
-    private void OnDisable()
+    public void OnLeft(InputAction.CallbackContext context)
     {
-        Left.started -= ChangeCharacterDown;
-        Right.started -= ChangeCharacterUp;
-        Left.Disable();
-        Right.Disable();
-    }
+        if (playerInput == null)
+        {
+            Debug.LogWarning("PlayerInput is null on OnLeft.");
+            return;
+        }
 
+        if (context.started)
+        {
+            Debug.Log($"Player {playerInput.playerIndex} pressed LEFT");
+            ChangeCharacterDown(context);
+        }
+    }
 
     public void ChangeCharacterUp(InputAction.CallbackContext context)
     {
-        characters[currentIndex].SetActive(false); // Désactive le personnage actuel
-        currentIndex = (currentIndex + 1) % characters.Count; // Incrémente et boucle
-        UpdateCharacterVisibility();
+        if (playerInput.playerIndex == 0)
+        {
+            charactersPlayer2[currentIndexPlayer2].SetActive(false);
+            currentIndexPlayer2 = (currentIndexPlayer2 + 1) % charactersPlayer2.Count;
+            UpdateCharacterVisibility2();
+        }
+        else if (playerInput.playerIndex == 1)
+        {
+            charactersPlayer3[currentIndexPlayer3].SetActive(false);
+            currentIndexPlayer3 = (currentIndexPlayer3 + 1) % charactersPlayer3.Count;
+            UpdateCharacterVisibility3();
+        }
     }
 
     public void ChangeCharacterDown(InputAction.CallbackContext context)
     {
-        characters[currentIndex].SetActive(false); // Désactive le personnage actuel
-        currentIndex = (currentIndex - 1 + characters.Count) % characters.Count; // Décrémente et boucle
-        UpdateCharacterVisibility();
+        if (playerInput.playerIndex == 0)
+        {
+            charactersPlayer2[currentIndexPlayer2].SetActive(false); // Désactive le personnage actuel
+            currentIndexPlayer2 = (currentIndexPlayer2 - 1 + charactersPlayer2.Count) % charactersPlayer2.Count; // Décrémente et boucle
+            UpdateCharacterVisibility2();
+        }
+
+        if (playerInput.playerIndex == 1)
+        {
+            charactersPlayer3[currentIndexPlayer3].SetActive(false); // Désactive le personnage actuel
+            currentIndexPlayer3 = (currentIndexPlayer3 - 1 + charactersPlayer3.Count) % charactersPlayer3.Count; // Décrémente et boucle
+            UpdateCharacterVisibility3();
+        }
+
     }
 
-    private void UpdateCharacterVisibility()
+    private void UpdateCharacterVisibility2()
     {
         // Désactive tous les personnages pour éviter les doublons
-        foreach (var character in characters)
+        foreach (var character in charactersPlayer2)
         {
             character.SetActive(false);
         }
 
         // Active uniquement le personnage courant
-        characters[currentIndex].SetActive(true);
+        charactersPlayer2[currentIndexPlayer2].SetActive(true);
+    }
+
+
+    private void UpdateCharacterVisibility3()
+    {
+        // Désactive tous les personnages pour éviter les doublons
+        foreach (var character in charactersPlayer3)
+        {
+            character.SetActive(false);
+        }
+
+        // Active uniquement le personnage courant
+        charactersPlayer3[currentIndexPlayer3].SetActive(true);
     }
 }
